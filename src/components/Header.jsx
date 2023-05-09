3892383d17d888aa71db2c20b5a642363027d806
nnatroo <nugonatroshvili@gmail.com>
2023-05-10 02:17:04 +0400

    added burger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,28 @@
-import React from "react";
-import classes from '../modules/Header.module.css'
-import CompanyLogo from '../assets/companyLogo.svg'
-import SearchIcon from '../assets/searchIcon.svg'
-import BurgerMenu from '../assets/menuBurger.svg'
+import React, { useState } from "react";
+import classes from "../modules/Header.module.css";
+import CompanyLogo from "../assets/companyLogo.svg";
+import SearchIcon from "../assets/searchIcon.svg";
+import BurgerMenuIcon from "../assets/menuBurger.svg";
+import BurgerMenu from "./BurgerMenu";
 
 const Header = () => {
+  const [showBurgerMenu, setShowBurgerMenu] = useState(false);
+
+  const burgerClickHandler = () => {
+    setShowBurgerMenu(!showBurgerMenu);
+    document.body.style.overflow = 'hidden';
+  };
+
+  const cancelHandler = () => {
+    setShowBurgerMenu(false);
+    document.body.style.overflow = 'visible';
+  }
+
   return (
     <>
-      <section className={classes['header']}>
-        <div className={classes['navbar-wrapper']}>
+      {showBurgerMenu && <BurgerMenu onCancel={cancelHandler} />}
+      <section className={classes["header"]}>
+        <div className={classes["navbar-wrapper"]}>
           <div className={classes["navbar-brand"]}>
             <img src={CompanyLogo} alt="CompanyLogo" />
             <h3>
@@ -30,7 +44,7 @@ const Header = () => {
           </div>
 
           <div className={classes["navbar-burger-menu-wrapper"]}>
-            <img src={BurgerMenu} alt="" />
+            <img onClick={burgerClickHandler} src={BurgerMenuIcon} alt="" />
           </div>
         </div>
       </section>
