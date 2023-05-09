0c92eecbe31e2f3fcbc29e83f30be51e3121c3d6
nnatroo <nugonatroshvili@gmail.com>
2023-05-09 18:45:21 +0400

    modified:   src/components/Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,31 +3,32 @@ import classes from "../modules/Services.module.css";
 import FirstServiceLogo from "../assets/firstServiceLogo.svg";
 import SecondServiceLogo from "../assets/secondServiceLogo.svg";
 import ThirdServiceLogo from "../assets/thirdServiceLogo.svg";
-import { ScrollReveal } from "reveal-on-scroll-react";
+import { Fade } from "react-awesome-reveal";
 
 const Services = () => {
   return (
-    <ScrollReveal.div className={classes["services-wrapper"]}>
-        
-      <h1>Services</h1>
+    <Fade>
+      <div className={classes["services-wrapper"]}>
+        <h1>Services</h1>
 
-      <div className={classes["services-items"]}>
-        <div className={classes["col"]}>
-          <img src={FirstServiceLogo} alt="" />
-          <h3 className={classes["title"]}>Service Name</h3>
-        </div>
+        <div className={classes["services-items"]}>
+          <div className={classes["col"]}>
+            <img src={FirstServiceLogo} alt="" />
+            <h3 className={classes["title"]}>Service Name</h3>
+          </div>
 
-        <div className={classes["col"]}>
-          <img src={SecondServiceLogo} alt="" />
-          <h3>Service Name</h3>
-        </div>
+          <div className={classes["col"]}>
+            <img src={SecondServiceLogo} alt="" />
+            <h3>Service Name</h3>
+          </div>
 
-        <div className={classes["col"]}>
-          <img src={ThirdServiceLogo} alt="" />
-          <h3>Service Name</h3>
+          <div className={classes["col"]}>
+            <img src={ThirdServiceLogo} alt="" />
+            <h3>Service Name</h3>
+          </div>
         </div>
       </div>
-    </ScrollReveal.div>
+    </Fade>
   );
 };
 
