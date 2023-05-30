7fd5cb41a358aaf1c948e44efab0e6fcc34bc181
nnatroo <nugonatroshvili@gmail.com>
2023-05-30 16:58:02 +0400

    Modified: Finished swiper

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -13,10 +13,16 @@ import Image1 from "../assets/image1.png";
 import Image2 from "../assets/image2.png";
 import Image3 from "../assets/image3.png";
 import Image4 from "../assets/image4.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const News = () => {
   const [activeSlide, setActiveSlide] = useState(0);
+
+  const slideChangeHandler = (swiper) => {
+    if (swiper.activeIndex == 5) {
+      setActiveSlide(0);
+    }
+  };
 
   const swiperCardData = [
     {
@@ -54,118 +60,102 @@ const News = () => {
       date: "19 April 2023",
       imageUrl: Image3,
     },
+    {
+      heading: "",
+      date: "",
+      imageUrl: "",
+    },
+    {
+      heading: "",
+      date: "",
+      imageUrl: "",
+    },
+    {
+      heading: "",
+      date: "",
+      imageUrl: "",
+    },
+    {
+      heading: "",
+      date: "",
+      imageUrl: "",
+    },
+    {
+      heading: "",
+      date: "",
+      imageUrl: "",
+    },
   ];
-
-  const swiperBreakpoints = {
-    0: {
-      spaceBetween: 200,
-      slidesPerView: 1,
-    },
-    365: {
-      spaceBetween: 200,  
-      slidesPerView: 1,
-    },
-    420: {
-      spaceBetween: 200,
-      slidesPerView: 1,
-    },
-    576: {
-      spaceBetween: 200,
-      slidesPerView: 1.5,
-    },
-    768: {
-      spaceBetween: -160,
-      slidesPerView: 1,
-    },
-    1024: {
-      spaceBetween: 400,
-      slidesPerView: 2.5,
-    },
-    1280: {
-      spaceBetween: 400,
-      slidesPerView: 2.5,
-    },
-    1366: {
-      spaceBetween: 350,
-    },
-    1440: {
-      spaceBetween: 250,
-    },
-    1680: {
-      spaceBetween: 0,
-    },
-    1920: {
-      spaceBetween: -100,
-    },
-  };
 
   return (
     // <Fade>
-      <div id="projects" className={classes["news-wrapper"]}>
-        <div className={`${classes["container"]} ${layout["container"]}`}>
-          <div className={classes["top-bar"]}>
-            <h2>News</h2>
-            <div className={classes["arrows-wrapper"]}>
-              <img
-                className={`swipper-prev-button ${classes["swiper-button-prev"]}`}
-                src={RightArrow}
-                alt="left-arrow"
-              />
+    <div id="projects" className={classes["news-wrapper"]}>
+      <div className={`${classes["container"]} ${layout["container"]}`}>
+        <div className={classes["top-bar"]}>
+          <h2>News</h2>
+          <div className={classes["arrows-wrapper"]}>
+            <img
+              className={`swipper-prev-button ${classes["swiper-button-prev"]}`}
+              src={RightArrow}
+              alt="left-arrow"
+            />
 
-              <img
-                className={`swipper-next-button ${classes["swiper-button-next"]}`}
-                src={RightArrow}
-                alt="right-arrow"
-              />
-            </div>
-          </div>
-
-          <div className={classes["swiper"]}>
-            <Swiper
-              className={classes["swiper-wrapper"]}
-              modules={[Navigation, Pagination, Scrollbar, A11y]}
-              rewind={true}
-              onSlideChange={(swiper) => {
-                setActiveSlide(swiper.activeIndex);
-                console.log(swiper.activeIndex);
-              }}
-              // onSwiper={(swiper) => console.log(swiper)}
-              spaceBetween={0}
-              // breakpoints={swiperBreakpoints}
-              slidesPerView={"auto"}
-              navigation={{
-                prevEl: ".swipper-prev-button",
-                nextEl: ".swipper-next-button",
-              }}
-            >
-              {swiperCardData.map((item, index) => (
-                <SwiperSlide className={`${classes["swiper-slide"]}`} key={index}>
-                  <div className={`${classes["swiper-card"]}`}>
-                    <div
-                      className={`${classes["image-container"]} ${
-                        activeSlide == index && classes["active"]
-                      }`}
-                    >
-                      <img
-                        className={classes["swiper-card-img"]}
-                        src={item.imageUrl}
-                        alt=""
-                      />
-                    </div>
-                    <div className={classes["swiper-card-content"]}>
-                      <h3>{item.heading}</h3>
-                      <p>{item.date}</p>
-                      <button>
-                        See More <img src={Arrow} alt="" />
-                      </button>
-                    </div>
-                  </div>
-                </SwiperSlide>
-              ))}
-            </Swiper>
+            <img
+              className={`swipper-next-button ${classes["swiper-button-next"]}`}
+              src={RightArrow}
+              alt="right-arrow"
+            />
           </div>
         </div>
+
+        <div className={classes["swiper"]}>
+          <Swiper
+            className={classes["swiper-wrapper"]}
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            rewind={true}
+            onSlideChange={(swiper) => {
+              setActiveSlide(swiper.activeIndex);
+              slideChangeHandler(swiper);
+              if (swiper.activeIndex >= 5 ) {
+                swiper.slideTo(0);
+              } 
+              console.log(swiper.activeIndex);
+            }}
+            spaceBetween={0}
+            slidesPerView={"auto"}
+            navigation={{
+              prevEl: ".swipper-prev-button",
+              nextEl: ".swipper-next-button",
+            }}
+          >
+            {swiperCardData.map((item, index) => (
+              <SwiperSlide className={`${classes["swiper-slide"]}`} key={index}>
+                <div className={`${classes["swiper-card"]}`}>
+                  <div
+                    className={`${classes["image-container"]} ${
+                      activeSlide == index && classes["active"]
+                    }`}
+                  >
+                    <img
+                      className={classes["swiper-card-img"]}
+                      src={item.imageUrl}
+                      alt=""
+                    />
+                  </div>
+                  <div className={classes["swiper-card-content"]}>
+                    <h3>{item.heading}</h3>
+                    <p>{item.date}</p>
+                    <button>
+                      See More <img src={Arrow} alt="" />
+                    </button>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </div>
+    </div>
     // </Fade>
   );
 };
